1c29364802991188cc486a8d4d6f60c53821f8f7
kyowoii23 <{kyowoii23@gmail.com}>
2022-11-26 17:26:01 +0900

    웹소켓을 사용해 프론트엔드와 연결하기

--- src/main.js
+++ src/main.js
@@ -3,17 +3,55 @@
 const Koa = require('koa')
 const Pug = require('koa-pug')
 const path = require('path')
+const route = require('koa-route')
+const serve = require('koa-static')
+const mount = require('koa-mount')
+const websockify = require('koa-websocket')
 
-const app = new Koa()
+const app = websockify(new Koa())
 
 // @ts-ignore
 new Pug({
   viewPath: path.resolve(__dirname, './views'),
-  app, // Binding `ctx.render()`, equals to pug.use(app)
+  app,
 })
+
+app.use(mount('/public', serve('src/public')))
 
 app.use(async (ctx) => {
   await ctx.render('main')
 })
+
+// Using routes
+app.ws.use(
+  route.all('/ws', (ctx) => {
+    // `ctx` is the regular koa context created from the `ws` onConnection `socket.upgradeReq` object.
+    // the websocket is added to the context on `ctx.websocket`.
+    // ctx.websocket.send('Hello World')
+    ctx.websocket.on('message', (data) => {
+      // do something with the message from client
+      if (typeof data.toString() !== 'string') {
+        return
+      }
+
+      const { message, nickname } = JSON.parse(data.toString())
+
+      const { server } = app.ws
+
+      if (!server) {
+        return
+      }
+
+      server.clients.forEach((client) => {
+        client.send(
+          JSON.stringify({
+            message,
+            nickname,
+          })
+        )
+      })
+    })
+  })
+)
 
 app.listen(4000)
---
+++ src/public/client.js
@@ -0,0 +1,68 @@
+// @ts-check
+
+// IIFE
+;(() => {
+  const socket = new WebSocket(`ws://${window.location.host}/ws`)
+  const formEl = document.getElementById('form')
+  /** @type {HTMLInputElement | null} */
+  // @ts-ignore
+  const inputEl = document.getElementById('input')
+  const chatsEl = document.getElementById('chats')
+
+  if (!formEl || !inputEl || !chatsEl) {
+    throw new Error('Init failed')
+  }
+
+  /**
+   * @typedef Chat
+   * @property {string} nickname
+   * @property {string} message
+   */
+
+  /**
+   * @type {Chat[]}
+   */
+  const chats = []
+
+  const adjectives = ['멋진', '훌륭한', '친절한', '새침한']
+  const animals = ['물범', '사자', '사슴', '돌고래', '독수리']
+
+  /**
+   *
+   * @param {string[]} array
+   * @returns {string}
+   */
+  function pickRandom(array) {
+    const randomIdx = Math.floor(Math.random() * array.length)
+    const result = array[randomIdx]
+    if (!result) {
+      throw new Error('array length is 0.')
+    }
+    return result
+  }
+
+  const myNickname = `${pickRandom(adjectives)} ${pickRandom(animals)}`
+
+  formEl.addEventListener('submit', (event) => {
+    event.preventDefault()
+    socket.send(
+      JSON.stringify({
+        nickname: myNickname,
+        message: inputEl.value,
+      })
+    )
+    inputEl.value = ''
+  })
+
+  socket.addEventListener('message', (event) => {
+    chats.push(JSON.parse(event.data))
+
+    chatsEl.innerHTML = ''
+
+    chats.forEach(({ message, nickname }) => {
+      const div = document.createElement('div')
+      div.innerText = `${nickname}: ${message}`
+      chatsEl.appendChild(div)
+    })
+  })
+})()
